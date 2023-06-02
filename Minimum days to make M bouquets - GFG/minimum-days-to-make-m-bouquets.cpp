99026ad0d550fa82c3915ef754d3b85a8a846b68
keshavsingla015 <97163964+keshavsingla015@users.noreply.github.com>
2023-06-02 15:17:18 +0530

    Added solution - LeetHub

---
+++ Minimum days to make M bouquets - GFG/minimum-days-to-make-m-bouquets.cpp
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool inspect(int M , int K , vector<int> &bloomDay , int mid){
+        int bloomed = 0 ; 
+        int total = 0 ; 
+        
+        for(int i = 0 ; i < bloomDay.size() ;i++){
+            if(bloomDay[i] <= mid){
+                bloomed++ ; 
+                if(bloomed == K){
+                    total++ ; 
+                    bloomed = 0 ; 
+                }
+            }
+            else{
+                bloomed = 0 ; 
+            }
+            if(total >= M){
+                return true ; 
+            }
+        }
+        return false ; 
+    }
+    
+    int solve(int M, int K, vector<int> &bloomDay){
+      // Code here+
+      if(bloomDay.size() < M*K) return -1 ; 
+      
+      int low = 0 , high = INT_MAX -1; 
+      int res = -1 ;
+      while(low <= high){
+          int mid = (low+high)/2 ; 
+          
+          if(inspect(M , K , bloomDay , mid)){
+              res = mid ; 
+              high = mid - 1 ;
+          }
+          else{
+              low = mid + 1 ; 
+          }
+      }
+      return res ; 
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int m, k;
+    cin >> m >> k;
+    string s;
+    getline(cin, s);
+    getline(cin, s);
+    int n = s.length();
+    int num = 0;
+    bool f = 0;
+    vector<int> bloomDay;
+    for (int i = 0; i < n; i++)
+    {
+      if (s[i] == ' ')
+      {
+        if (!f)
+          continue;
+        bloomDay.push_back(num);
+        num = 0;
+        f = 0;
+      }
+      else
+      {
+        num = (num * 10) + (s[i] - '0');
+        f = 1;
+      }
+    }
+    if (f)
+    {
+      bloomDay.push_back(num);
+    }
+    Solution obj;
+    cout<<obj.solve(m, k, bloomDay)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
